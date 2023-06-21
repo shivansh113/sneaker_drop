2dba7df3cb6d4a3b0d167a6e3dd5c6effeb86bb0
Shivansh Dutt <shivanshd@gmail.com>
2023-06-21 02:04:09 -0600

    added the auto scroll feature using selenium

---
+++ nike_crawler.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+
+driver = webdriver.Chrome()
+
+url = 'https://www.nike.com/ca/w/new-shoes-3n82yzy7ok'
+
+driver.get(url)
+
+num_scrolls = 5 
+
+for i in range(num_scrolls):
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+    time.sleep(1.5)
+
+page_source = driver.page_source
+
+soup = BeautifulSoup(page_source, 'html.parser')
+
+items = soup.find_all('div', {'class': 'product-card__body'})
+prices = soup.find_all('div', {'class': 'product-card__price'})
+
+item_store = []
+price_store = []
+
+for item in items:
+  item_store.append(item.find('a', {'class': 'product-card__link-overlay'}).text)
+
+for price in prices:
+  price_store.append(price.find('div', {'class': 'product-price ca__styling is--current-price css-11s12ax'}).text)
+
+
+for i in range(len(item_store)):
+  print(i+1,': ', item_store[i], " ", price_store[i])

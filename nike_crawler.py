5fcd298e3ee50ea9c59819fac3610ea5a0fdb8e1
Shivansh Dutt <shivanshd@gmail.com>
2023-06-21 02:14:19 -0600

    solving conflicts in merge

--- nike_crawler.py
+++ nike_crawler.py
@@ -36,18 +36,3 @@ for price in prices:
 for i in range(len(item_store)):
   print(i+1,': ', item_store[i], " ", price_store[i])
 
-items = soup.find_all('div', {'class': 'product-card__body'})
-prices = soup.find_all('div', {'class': 'product-card__price'})
-
-item_store = []
-price_store = []
-
-for item in items:
-  item_store.append(item.find('a', {'class': 'product-card__link-overlay'}).text)
-
-for price in prices:
-  price_store.append(price.find('div', {'class': 'product-price ca__styling is--current-price css-11s12ax'}).text)
-
-
-for i in range(len(item_store)):
-  print(i+1,': ', item_store[i], " ", price_store[i])
